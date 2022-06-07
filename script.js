f417439050451491ef921dc373e222cdb2fd0a84
nisa0602 <nisahidayatika999@gmail.com>
2022-06-07 14:39:34 +0700

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let form = document.getElementById("form");
-let input = document.getElementById("input");
+// let input = document.getElementById("input");
 // let list = document.getElementById("list");
-let name1 = document.getElementById("name1");
+// let name1 = document.getElementById("name1");
 
 //Retrieve the data
-function acceptData(){
-    let data = {};
-    data["text"] = input.value;
-    return data;
-}
+// function acceptData(){
+//     let data = " ";
+//     data["value"] = input.value;
+//     return data;
+// }
 
 //submit function
 // function formSubmit(e) {
@@ -16,3 +16,18 @@ function acceptData(){
 //     let data = acceptData();
 //     console.log("clicked")
 // }
+
+// let showData = () => {
+//     input.addEventListener("click", e => {
+//         e.preventDefault();
+//         document.getElementById("name1").innerHTML = data;
+
+//         console.log("oke")
+//         acceptData();
+//     })
+// }  
+
+function showData(){
+    let input = document.getElementById("input").value;
+    alert(input);
+}
